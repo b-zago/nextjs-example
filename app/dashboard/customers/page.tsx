0feb33e46f8384e8addf565934bbe0661fa0c62b
b-zago <bart.zagozdon@gmail.com>
2025-10-19 14:26:22 +0200

    dynamic tests

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,3 @@
 export default async function Page() {
-    const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-    await sleep(4000);
   return <p>Customers Page</p>;
 }
